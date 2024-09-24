3cc3f6c054bd71fb3b8d65d195a1f9f5814ae89d
MRIDUL896 <156008897+MRIDUL896@users.noreply.github.com>
2024-09-25 01:07:25 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,15 +4,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "15d",
+    });
 
-	console.log(token)
+    console.log(token);
 
-	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000,
-	});
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        httpOnly: true,  // Prevents client-side access to the cookie
+        secure: true,  // Set to true if in production
+        sameSite: 'strict',  // Controls cookie sharing across sites
+    });
 };
 
 module.exports = generateTokenAndSetCookie;
