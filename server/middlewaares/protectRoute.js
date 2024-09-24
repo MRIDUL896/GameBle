8ffb39e81c5d663c670efce84076a10e4ed2f43c
MRIDUL896 <156008897+MRIDUL896@users.noreply.github.com>
2024-09-25 02:31:42 +0530

    Revert "Update protectRoute.js"
    This reverts commit c124cc3047ebd696cc4dc358f36068da1bfff384.

--- server/middlewaares/protectRoute.js
+++ server/middlewaares/protectRoute.js
@@ -4,7 +4,7 @@ const userModel = require('../models/userModel')
 const protectRoute = async (req,res,next) => {
     try{
         const token = req.cookies.jwt;
-        console.log(req)
+        console.log(req.cookies)
         if(!token){
             return res.status(401).json({err : "Unauthorised"});
         }
