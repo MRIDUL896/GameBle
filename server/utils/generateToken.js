5f918e2fa15254347ecf499f27cbd37a5fd04213
MRIDUL896 <156008897+MRIDUL896@users.noreply.github.com>
2024-09-25 00:47:24 +0530

    deployment testing

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -11,10 +11,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 	console.log(token)
 
 	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development",
+		maxAge: 15 * 24 * 60 * 60 * 1000,
 	});
 };
 
