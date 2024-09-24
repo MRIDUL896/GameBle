ab9cfbecf58f68e751ef7e26f1420255f201a448
MRIDUL896 <156008897+MRIDUL896@users.noreply.github.com>
2024-09-25 00:29:32 +0530

    final commit for deployment

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:["https://gameable-frontend.onrender.com/"],
+        origin:["https://gameable-frontend.onrender.com"],
         methods:["GET","POST","PUT","DELETE"]
     }
 });
